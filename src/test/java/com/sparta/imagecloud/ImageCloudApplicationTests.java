7b88ffdf9c40f4e940dd44f5bf463793f0625da6
thethe133@naver.com <thethe133@naver.com>
2022-02-12 12:09:27 +0900

    Initial commit

---
+++ src/test/java/com/sparta/imagecloud/ImageCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.imagecloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
